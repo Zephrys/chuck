03e23075f883b2fbc064858f149bf8ad51d3b31a
Gyanendra Mishra <anomaly.the@gmail.com>
2016-03-06 18:58:42 +0530

    initialized and added prototypes

---
+++ chuck/chuck.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from docopt import docopt
+
+
+def parse_aftership():
+	pass
+
+def add_couriers():
+	pass
+
+def main():
+	'''chuck takes care of your shipments'''
